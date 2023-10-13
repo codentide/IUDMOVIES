494737363e60f8cbb9dddd0fc50b3ae396a5364a
Marco Del Boccio <marcodelboccio77@gmail.com>
2023-10-13 15:57:14 -0500

    Finishing productora and genero crud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,13 @@
-import logo from "./logo.svg";
+import Genero from "./components/generos/Genero";
 import "./App.css";
-import { axiosConfig } from "./config/axiosConfig";
-import { useEffect, useState } from "react";
+import Header from "./components/gui/Header";
+import Modal from "./components/gui/Modal";
+import AppRouter from "./routers/AppRouter";
 
 function App() {
-  const [generos, setGeneros] = useState([]);
-
-  useEffect(() => {
-    listGeneros();
-  }, []);
-
-  const listGeneros = async () => {
-    const response = await axiosConfig.get("generos/", {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    setGeneros(response.data);
-  };
-
   return (
     <div className="App">
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Nombre</th>
-            <th>Estado</th>
-            <th>Descripción</th>
-          </tr>
-        </thead>
-        <tbody>
-          {generos.map((genero, index) => {
-            return (
-              <tr>
-                <td>{index + 1}</td>
-                <td>{genero.nombre}</td>
-                <td>{genero.estado ? "Activo" : "Inactivo"}</td>
-                <td>{genero.descripcion}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <AppRouter />
     </div>
   );
 }
